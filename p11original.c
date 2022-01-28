1b7ca396d1c7430f0ae0c99a842b2ea45d3879fe
Abhishek32971 <97088891+Abhishek32971@users.noreply.github.com>
2022-01-28 12:28:04 +0000

    new

--- p11original.c
+++ p11original.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-typedef struct _complex
-{ 
-  float real,imaginary;
-}Complex;
+struct _complex{ float real,imaginary;};
+typedef struct _complex Complex;
 int get_n()
 { 
   int n;
@@ -12,11 +10,11 @@ int get_n()
 }
 void input_n_complex(int n,Complex c[n])
 { 
-   for(int i=0;i<n;i++)
-     {
-        printf("enter the values\n");
-        scanf("%f%f",&c[i].real,&c[i].imaginary);
-     }
+  for(int i=0;i<n;i++)
+    {
+      printf("enter the values\n");
+      scanf("%f %f",&c[i].real,&c[i].imaginary);
+    }
 }
 Complex add_n_complex(int n,Complex c[n])
 { 
@@ -33,16 +31,16 @@ void output(int n,Complex c[n],Complex result)
 {
   int i;
     for(i=0;i<n-1;i++)
-      printf("%f + %f i +",c[i].real,c[i].imaginary);
-      printf(" %f + %f i",c[i].real,c[i].imaginary);
-     printf(" is %f + %f i\n",result.real,result.imaginary);
+      printf("%.0f + %.0fi +",c[i].real,c[i].imaginary);
+      printf(" %.0f + %.0fi",c[i].real,c[i].imaginary);
+      printf(" is %.0f + %.0fi\n",result.real,result.imaginary);
 }    
 int main()
 {
   Complex h[15];
- int x;
- x=get_n();
- input_n_complex(x,h);
- output(x,h,add_n_complex(x,h));
- return 0;
+  int x;
+  x=get_n();
+  input_n_complex(x,h);
+  output(x,h,add_n_complex(x,h));
+  return 0;
 }
